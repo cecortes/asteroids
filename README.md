e40588750a60bdc93f3e74e1bfd931ef1ca942fa
César López Cortés <cesarlopezcortes@hotmail.com>
2022-10-27 19:47:32 -0500

    Setup files and folders for Hostinger

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# asteroids
+# Asteroids mini game
+
 A mini game made with Phaser, HTML5 and JS
+
+## How to play
+
+- Use the arrow keys to move the ship
+- Use the space bar to shoot
+
+### Credits
+
+- [Phaser](https://phaser.io/)
+- [Cesar Lopez](www.cesarlopezcortes.com)
---
+++ js/minigame.js
@@ -0,0 +1,156 @@
+//* Config object, contains all the game settings *//
+var config = {
+  type: Phaser.AUTO,
+  width: 1200,
+  height: 800,
+  backgroundColor: "black",
+  parent: "asteroids-game",
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+      gravity: { y: 0 },
+    },
+  },
+  scene: [
+    {
+      preload: preload,
+      create: create,
+      update: update,
+      extend: {
+        generateAsteroids: generateAsteroids,
+      },
+    },
+  ],
+};
+
+//* Instance of the game *//
+var game = new Phaser.Game(config);
+
+//* Global variables *//
+var nave;
+var life = 3;
+var score = 0;
+var ammo = 10;
+var cursorLeft, cursorRight, cursorUp, cursorDown, cursorSpace;
+var velVessel = 500;
+var asteroids;
+var minAsteroids = 2;
+var maxAsteroids = 6;
+var accel = 5;
+var timeShowAsteroids = 1600;
+
+//* Preload function: loads all the assets *//
+function preload() {
+  // Load the spaceship image
+  this.load.image("vessel", "./../assets/vessel.png");
+
+  // Load the asteroid sprite sheet
+  this.load.spritesheet("asteroids", "./../assets/asteroid_sprite.png", {
+    frameWidth: 96,
+    frameHeight: 96,
+  });
+}
+
+//* Create function: creates all the game objects *//
+function create() {
+  // Create the spaceship
+  nave = this.physics.add.sprite(
+    game.config.width / 2,
+    game.config.height / 2,
+    "vessel"
+  );
+
+  // Set life, score and ammo
+  nave.life = life;
+  nave.score = score;
+  nave.ammo = ammo;
+
+  // Create the asteroids group
+  asteroids = this.physics.add.group({
+    defaultKey: "asteroids",
+    frame: 0,
+    maxSize: 50,
+  });
+
+  // Add new event to generate asteroids
+  this.time.addEvent({
+    delay: timeShowAsteroids,
+    loop: true,
+    callback: () => {
+      this.generateAsteroids();
+    },
+  });
+
+  // Set the cursor keys
+  cursorLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+  cursorRight = this.input.keyboard.addKey(
+    Phaser.Input.Keyboard.KeyCodes.RIGHT
+  );
+  cursorUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  cursorDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+  cursorSpace = this.input.keyboard.addKey(
+    Phaser.Input.Keyboard.KeyCodes.SPACE
+  );
+}
+
+//* Update function: updates the game objects *//
+function update() {
+  // Add acceleration to the asteroids
+  Phaser.Actions.IncY(asteroids.getChildren(), accel);
+
+  // Check if the spaceship is out of the screen
+  asteroids.children.iterate((asteroid) => {
+    if (asteroid.y > game.config.height) {
+      asteroids.killAndHide(asteroid);
+    }
+  });
+
+  // Set velocity to the spaceship
+  nave.setVelocity(0);
+
+  // Move the spaceship with the cursor keys
+  if (cursorLeft.isDown) {
+    nave.setVelocityX(-velVessel);
+  } else if (cursorRight.isDown) {
+    nave.setVelocityX(velVessel);
+  }
+
+  if (cursorUp.isDown) {
+    nave.setVelocityY(-velVessel);
+  } else if (cursorDown.isDown) {
+    nave.setVelocityY(velVessel);
+  }
+
+  // Shoot with the space key
+  if (cursorSpace.isDown) {
+    if (nave.ammo > 0) {
+      nave.ammo--;
+      console.log(nave.ammo);
+    }
+  }
+}
+
+//* Function to generate asteroids *//
+function generateAsteroids() {
+  // Create a random number of asteroids between minAsteroids and maxAsteroids
+  var numAsteroids = Phaser.Math.Between(minAsteroids, maxAsteroids);
+
+  // Create the asteroids
+  for (let x = 0; x < numAsteroids; x++) {
+    let asteroid = asteroids.get();
+
+    if (asteroid) {
+      asteroid.setActive(true);
+      asteroid.setVisible(true);
+      asteroid.setFrame(Phaser.Math.Between(0, 2));
+      asteroid.y = -192;
+      asteroid.x = Phaser.Math.Between(0, game.config.width);
+
+      // Avoid overlapping
+      this.physics.add.overlap(asteroid, asteroids, (asteroidOnCollision) => {
+        asteroidOnCollision.x = Phaser.Math.Between(0, game.config.width);
+      });
+    }
+  }
+}
